d1e518b68d8565c64341073d0691b8bc5c3480b9
Mac Siri <krairit.siri@gmail.com>
2018-05-24 15:33:36 -0400

    Improve formats in codeblock
    - Remove preserve_tags 
    - Adjust remove_inner_newlines

--- lib/reverse_markdown/converters/text.rb
+++ lib/reverse_markdown/converters/text.rb
@@ -28,10 +28,7 @@ module ReverseMarkdown
         text = remove_border_newlines(text)
         text = remove_inner_newlines(text)
         text = escape_keychars(text)
-
         text = preserve_keychars_within_backticks(text)
-        text = preserve_tags(text)
-
         text
       end
 
@@ -39,16 +36,12 @@ module ReverseMarkdown
         text.gsub(/\u00A0/, "&nbsp;")
       end
 
-      def preserve_tags(text)
-        text.gsub(/[<>]/, '>' => '\>', '<' => '\<')
-      end
-
       def remove_border_newlines(text)
         text.gsub(/\A\n+/, '').gsub(/\n+\z/, '')
       end
 
       def remove_inner_newlines(text)
-        text.tr("\n\t", ' ').squeeze(' ')
+        text.tr("\n\t", '')
       end
 
       def preserve_keychars_within_backticks(text)
